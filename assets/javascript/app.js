6107885424b296e34646647319bb510fd034f175
bsthomson <thomson.bs@gmail.com>
2018-06-16 12:20:28 -0500

    changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,18 @@
 
-$("#jene-vickrey").val()
+$("#submit").on("click", function() {
+    event.preventDefault();
+    var gifVal = $("#create-gif").val().trim();
+    console.log(gifVal);
+    var gifBut = $("<button>");
+    gifBut.addClass("btn btn-danger");
+    gifBut.attr("data-representative", gifVal);
+    gifBut.text(gifVal);
+    $("#buttons").append(gifBut);
+    
+})
 
-$(".btn").on("click", function() {
+$(document.body).on("click", ".btn-danger", function() {
+    event.preventDefault();
     var representative = $(this).attr("data-representative")
     console.log(this)
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + representative + "&api_key=r2yx4XalMqrmoLmjtkv0m8m6N7jiAMTC&limit=10";
