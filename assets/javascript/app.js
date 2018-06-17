f73f18aba499f4112a43c9e8595827cdb693069e
bsthomson <thomson.bs@gmail.com>
2018-06-17 09:22:56 -0500

    changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,20 +1,33 @@
+var topics = ["Jene Vickrey", "Melissa Rooker"];
+console.log(topics);
+
+function topicButtons() {
+    for (i=0; i < topics.length; i++){
+        var gifVal = topics[i];
+        var gifBut = $("<button>");
+        gifBut.addClass("btn btn-danger");
+        gifBut.attr("data-representative", gifVal);
+        gifBut.text(gifVal);
+        $("#buttons").append(gifBut);       
+    }
+    for (j=0; j < topics.length; j++){
+        topics.splice(0, topics.length);
+    }
+}
+topicButtons()
 
 $("#submit").on("click", function() {
     event.preventDefault();
-    var gifVal = $("#create-gif").val().trim();
-    console.log(gifVal);
-    var gifBut = $("<button>");
-    gifBut.addClass("btn btn-danger");
-    gifBut.attr("data-representative", gifVal);
-    gifBut.text(gifVal);
-    $("#buttons").append(gifBut);
+    var gifSub = $("#create-gif").val().trim();
+    console.log(gifSub);
+    topics.push(gifSub);
+    topicButtons();
     
 })
 
 $(document.body).on("click", ".btn-danger", function() {
     event.preventDefault();
     var representative = $(this).attr("data-representative")
-    console.log(this)
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + representative + "&api_key=r2yx4XalMqrmoLmjtkv0m8m6N7jiAMTC&limit=10";
     $.ajax({
         url: queryURL,
@@ -22,14 +35,18 @@ $(document.body).on("click", ".btn-danger", function() {
       })
     .then(function(response) {
         var results=response.data;
-        console.log(response);
         for (var i=0; i < results.length; i++) {
             
-            var gifDiv=$("<div class = 'item'>");
+            var gifDiv=$("<class = 'item'>");
             
             var representativeImage = $("<img>");
             
-            representativeImage.attr("src", results[i].images.fixed_height.url);
+            representativeImage.attr("src", results[i].images.fixed_width_still.url);
+            representativeImage.attr("data-still", results[i].images.fixed_width_still.url);
+            representativeImage.attr("data-animate", results[i].images.fixed_width.url);
+            representativeImage.attr("data-state", "still");
+            representativeImage.attr("class", "gif");
+
             
             gifDiv.prepend(representativeImage);
             
@@ -37,3 +54,14 @@ $(document.body).on("click", ".btn-danger", function() {
         }
     });
 });
+
+$(document.body).on("click", ".gif", function() {
+    var state= $(this).attr("data-state");
+    if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+    } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+    }
+})
