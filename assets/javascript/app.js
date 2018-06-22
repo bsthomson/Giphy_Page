12f6e6baa7a6db38962c4d48104f6c6bf574da39
bsthomson <thomson.bs@gmail.com>
2018-06-21 23:20:50 -0500

    changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,9 @@
+// Array for storing buttons
+
 var topics = ["Jene Vickrey", "Melissa Rooker"];
 console.log(topics);
 
+// Creates buttons
 function topicButtons() {
     for (i=0; i < topics.length; i++){
         var gifVal = topics[i];
@@ -13,6 +16,7 @@ function topicButtons() {
 }
 topicButtons()
 
+// When submit button is clicked pushes the value into the topics array
 $("#submit").on("click", function() {
     event.preventDefault();
     $("#buttons").empty();
@@ -26,18 +30,25 @@ $("#submit").on("click", function() {
     
 })
 
-$(document.body).on("click", ".btn-danger", function() {
-    event.preventDefault();
+// Variables for altering the offset value of the queryURL
+var limit = 10;
+var offset = 0;
+var page = 0;
+
+
+//Function for creating gifs
+function gifCreate(offsetVal) {
     var representative = $(this).attr("data-representative")
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + representative + "&api_key=r2yx4XalMqrmoLmjtkv0m8m6N7jiAMTC&limit=10";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + representative + "&api_key=r2yx4XalMqrmoLmjtkv0m8m6N7jiAMTC&limit="+limit+"&offset="+offsetVal;
     $.ajax({
         url: queryURL,
         method: "GET"
       })
     .then(function(response) {
+        console.log(queryURL);
         var results=response.data;
-        for (var i=0; i < results.length; i++) {
-            
+        // loops through all of the results
+        for (var i=0; i < results.length ; i++) {
             var gifDiv=$("<class = 'item'>");
             
             var representativeImage = $("<img>");
@@ -52,11 +63,32 @@ $(document.body).on("click", ".btn-danger", function() {
             
             $("#gifs-go-here").prepend(gifDiv);
             var p = $("<p>").text("Rating: " + results[i].rating);
-            $(gifDiv).append(p);  
-        }
-    });
+            $(gifDiv).append(p);   
+                 
+            
+            
+        };
+    });    
+}
+
+// When a red button is clicked creates its gif
+$(document.body).on("click", ".btn-danger", function() {
+    event.preventDefault();
+    offset = 0;
+    gifCreate(offset);
+
 });
 
+// When the 10 more button is clicked the offset is increased by 10 and the new gifs are created
+$(document.body).on("click", "#10more", function() {
+    event.preventDefault();
+    page++;
+    offset = page * limit;
+    gifCreate(offset);
+    console.log(page);         
+});
+
+// Switches the gifs from static to animated by switching their data-state and image src.
 $(document.body).on("click", ".gif", function() {
     var state= $(this).attr("data-state");
     if (state === "still") {
